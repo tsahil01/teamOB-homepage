8deadb5bbee7bd181ab7e85a9e24fbabba3e1ce2
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-01-12 21:33:16 +0530

    fix dir

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,12 +1,13 @@
-import { HeroSection } from "./nav/hero/hero";
+
+import { HeroSection } from "./hero/hero";
 import { Navbar } from "./nav/navbar";
 
 export function Home() {
     return <>
         <div className="flex flex-col h-screen w-screen">
             <main className="flex flex-col container mx-auto mt-9">
-                <Navbar/>
-                <HeroSection/>
+                <Navbar />
+                <HeroSection />
             </main>
         </div>
     </>
